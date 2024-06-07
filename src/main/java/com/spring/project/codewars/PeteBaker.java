4dca30060864596a328614aef5a9fa079cfe9dae
hellrider1990 <juanjoseguiradoruiz90@gmail.com>
2024-06-07 17:53:53 +0200

    PPasado el primer test, pero falla en el segundo

--- src/main/java/com/spring/project/codewars/PeteBaker.java
+++ src/main/java/com/spring/project/codewars/PeteBaker.java
@@ -1,6 +1,8 @@
 package com.spring.project.codewars;
 
-import java.util.*;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class PeteBaker {
 
@@ -19,6 +21,7 @@ public class PeteBaker {
 
         Map<String, Integer> ordenadoRecipe = new TreeMap<String, Integer>();
         Map<String, Integer> ordenadoAvailable = new TreeMap<String, Integer>();
+        Map<String, Integer> numRecetas = new TreeMap<String, Integer>();
 
         if (recipe.isEmpty() || available.isEmpty() || available.size() < recipe.size()) {
 
@@ -31,21 +34,48 @@ public class PeteBaker {
 
             Iterator<String> itReceta = ordenadoRecipe.keySet().iterator();
             Iterator<String> itAvailable = ordenadoAvailable.keySet().iterator();
+            Iterator<String> itNumReceta = ordenadoAvailable.keySet().iterator();
 
             while (itReceta.hasNext()) {
-                String key = itReceta.next();
-                Integer value = ordenadoRecipe.get(key);
-                System.out.println("Clave de receta " + key + " / " + "Valor receta " + value);
+                while (itAvailable.hasNext()) {
+
+                    String keyRecipe = itReceta.next();
+                    Integer valueRecipe = ordenadoRecipe.get(keyRecipe);
+                    System.out.println("Clave de receta " + keyRecipe + " / " + "Valor receta " + valueRecipe);
+
+                    String keyAvailable = itAvailable.next();
+                    Integer valueAvailable = ordenadoAvailable.get(keyAvailable);
+                    System.out.println("Clave de receta " + keyAvailable + " / " + "Valor receta " + valueAvailable);
+
+                    if (numRecetas.containsKey(keyRecipe) != numRecetas.containsKey(keyAvailable)) {
+
+                        return recetas;
+                    }
+
+                    Integer resultado = valueAvailable / valueRecipe;
+
+                    numRecetas.put(keyRecipe, resultado);
+                }
             }
 
-            while (itAvailable.hasNext()) {
-                String key = itAvailable.next();
-                Integer value = ordenadoAvailable.get(key);
-                System.out.println("Clave de receta " + key + " / " + "Valor receta " + value);
+            while (itNumReceta.hasNext()) {
+                String keyNumRecipe = itNumReceta.next();
+                Integer valueNumReceta = numRecetas.get(keyNumRecipe);
+
+                if (valueNumReceta == null) {
+
+                    recetas = 0;
+
+                    System.out.println("\n-----> Clave de receta: " + keyNumRecipe + " / " + "Valor receta " + valueNumReceta);
+                } else {
+
+
+                    System.out.println("\n-----> Clave de receta: " + keyNumRecipe + " / " + "Valor receta " + valueNumReceta);
+                }
             }
 
-            return recetas;
         }
+        return recetas;
     }
 }
 
@@ -54,16 +84,14 @@ class Prueba {
     public static void main(String[] args) {
 
         Map<String, Integer> recipe = Map.of(
-                "harina", 200,
-                "azúcar", 100,
-                "huevos", 300
-        );
-
+                "flour", 500,
+                "sugar", 200,
+                "eggs", 1);
         Map<String, Integer> available = Map.of(
-                "harina", 2000,
-                "azúcar", 2000,
-                "leche", 3000
-        );
+                "flour", 1200,
+                "sugar", 1200,
+                "eggs", 5,
+                "milk", 200);
 
         System.out.println(PeteBaker.cakes(recipe, available));
     }
